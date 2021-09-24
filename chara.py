17d7850ae00104d5059ac581c2f0092e2b593b79
caffeine <64718708+caffeiness@users.noreply.github.com>
2021-09-24 23:59:18 +0900

    Update chara.py

--- chara.py
+++ chara.py
@@ -7,7 +7,7 @@ gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
-contours = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
